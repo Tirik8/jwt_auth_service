0084f823b12a3d66abeef2b8bc9d54b23311940c
Tirik8 <yaroslavsemenoviop@gmail.com>
2025-05-22 19:36:17 +0300

    visible alembic

--- migrations/env.py
+++ migrations/env.py
@@ -4,10 +4,10 @@ from alembic import context
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from app.db.database import Base
 from app.core.config import settings
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 config = context.config
 
