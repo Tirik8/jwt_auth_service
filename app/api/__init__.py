f497d6f9dee4e03b07bbf5603955276275a63a30
Tirik8 <yaroslavsemenoviop@gmail.com>
2025-05-22 17:38:46 +0300

    change including root router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
 
 from . import v1
-from .root import router as root_router
 
 router = APIRouter()
 
 router.include_router(v1.router, prefix="/v1")
-router.include_router(root_router, prefix="/")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app import api
+from app.api.root import router as root_router
 from app.db.database import engine, Base
 
 Base.metadata.create_all(bind=engine)
@@ -8,4 +9,4 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(api.router, prefix="/api")
-
+app.include_router(root_router, prefix="")
